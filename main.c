4174cd01cd11b2bdf8fbe8f23a12f133d3d45255
piotrw777 <piotr.wasilewski2@gmail.com>
2021-05-07 08:30:41 +0200

    file counter

--- main.c
+++ main.c
@@ -1,19 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
 
+//#define COUNTER_FILE "/tmp/counter.txt"
+#define COUNTER_FILE "/mnt/b/C++/messengers/counter.txt"
+
 void * threadFunction(void * arg)
 {
-    int start = *((int *) arg);
+    unsigned long long k;
+    char c = 'A';
+
+
+    //open the file
+    FILE *file = fopen(COUNTER_FILE, "r+");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error opening the file");
+        exit(1);
+    }
+    printf("Created the file %s\n", COUNTER_FILE);
+
+    //checking if the file exists
+    if ( !fread(&c, sizeof(c), 1, file) )
+    {
+        printf("File is empty!!!\n");
+
+    }
+
+
     int n = 10;
     while (n--)
     {
-        printf("%d\n", start);
-        start++;
+        //read current value
+        fread(&c, sizeof(c), 1, file);
+        //fread(buffer, sizeof(*buffer), 32, file);
+        //sprintf(k_string,"%lld", k);
+        printf("Value read: %c\n", c);
+        //increment value
+        c++;
+
+        //write new value
+        //fwrite(&c, sizeof(c), 1, file);
+        fprintf(file, "%c\n", c);
+        printf("Value written: %c\n", c);
+
         sleep(1);
     }
-
-   pthread_exit(NULL);
+    pthread_exit(NULL);
+    fclose(file);
 
 }
 
@@ -24,5 +59,4 @@ int main()
     pthread_t w;
     pthread_create(&w, NULL, threadFunction, (void *) &start);
     pthread_join(w, NULL);
-
 }
